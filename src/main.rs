2e0acdd5acf16fef0e89ab25a7c1d2dcb65ef9ae
Chris Aumann <me@chr4.org>
2017-07-22 03:17:19 +0200

    Use multiple threads

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ extern crate futures;
 extern crate tokio_core;
 
 use std::io;
+use std::thread;
+use std::sync::{Arc, Mutex};
 use futures::Future;
 use futures::stream::Stream;
-use hyper::{Client, Request, Method};
+use hyper::{Uri, Client, Request, Method};
 use hyper::header::{UserAgent, SetCookie};
 use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
@@ -21,22 +23,52 @@ fn main() {
     let user_agent_desktop = "Googlebot (cache warmer)";
     let user_agent_mobile = "Googlebot Android Mobile (cache warmer)";
 
+    let uris = Arc::new(Mutex::new(vec![]));
+    let lines = lines_from_file("urls.txt").unwrap();
+
+    // Collect lines, enqueue for workers
+    for l in lines {
+        let line = l.unwrap();
+        let uri: Uri = format!("https://chr4.org/{}", line).parse().unwrap();
+
+        let mut input = uris.lock().unwrap();
+        input.push(uri);
+    }
+
+    let handles: Vec<_> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        .iter()
+        .map(|_| {
+            let x = uris.clone();
+            thread::spawn(move || {
+                spawn_worker(x, UserAgent::new(user_agent_desktop));
+            })
+        })
+        .collect();
+
+    // Block until all work is done
+    for h in handles {
+        h.join().unwrap();
+    }
+}
+
+fn spawn_worker(uris: Arc<Mutex<Vec<Uri>>>, user_agent: UserAgent) {
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
     let client = Client::configure()
         .keep_alive(true)
-        .connector(HttpsConnector::new(threads, &handle).unwrap())
+        .connector(HttpsConnector::new(4, &handle).unwrap())
         .build(&handle);
 
-    let lines = lines_from_file("urls.txt").unwrap();
+    loop {
+        let uri = match get_next_uri(&uris) {
+            Some(uri) => uri,
+            None => break,
+        };
 
-    for l in lines {
-        let line = l.unwrap();
-        let uri = format!("https://chr4.org/{}", line).parse().unwrap();
-
-        let mut req: hyper::Request = Request::new(Method::Get, uri);
-        req.headers_mut().set(UserAgent::new(user_agent_desktop));
+        // TODO: Can clone() be ommitted somehow?
+        let mut req: hyper::Request = Request::new(Method::Get, uri.clone());
+        req.headers_mut().set(user_agent.clone());
 
         req.headers_mut().set(SetCookie(
             vec![String::from("cacheupdate=true")],
@@ -45,7 +77,7 @@ fn main() {
         let work = client.request(req).and_then(|res| {
             println!(
                 "{}: {} {:?}",
-                line,
+                uri,
                 res.status(),
                 res.headers().get::<XCacheStatus>()
             );
@@ -56,6 +88,12 @@ fn main() {
 
         core.run(work).unwrap();
     }
+}
+
+// Returns next URI without blocking the mutex for longer than necessary
+fn get_next_uri(uris: &Arc<Mutex<Vec<Uri>>>) -> Option<Uri> {
+    let mut uris = uris.lock().unwrap();
+    uris.pop()
 }
 
 
